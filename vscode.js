0c7b28b8d215abf6a12e77be5fdce38993d418d1
Michal Honc <honc355@gmail.com>
2018-08-15 21:05:06 +0200

    init

---
+++ vscode.js
@@ -0,0 +1,53 @@
+{
+    "window.zoomLevel": 0,
+    "window.menuBarVisibility": "default",
+    "editor.minimap.enabled": false,
+    "files.exclude": {
+        "**/.git": true,
+        "**/.DS_Store": true,
+        "jspm_packages": true,
+        ".idea": true
+    },
+    "editor.fontFamily": "Consolas, 'Courier New', monospace",
+    "editor.tabSize": 3,
+    "editor.wordWrap": "on",
+    "workbench.iconTheme": "vscode-icons",
+    "vsicons.dontShowNewVersionMessage": true,
+    "workbench.activityBar.visible": false,
+    "terminal.integrated.shell.windows": "C:\\Program Files\\Git\\bin\\bash.exe",
+    "files.autoSave": "off",
+    "eslint.options": {
+        "eslint.autoFixOnSave": true
+    },
+    "emmet.includeLanguages": {
+        "javascript": "javascriptreact"
+    },
+    "editor.snippetSuggestions": "top",
+    "window.title": "${dirty} ${activeEditorMedium}${separator}${rootName}",
+    "eslint.enable": true,
+    "eslint.validate": [
+        "javascript",
+        "javascriptreact"
+    ],
+    "eslint.autoFixOnSave": true,
+    "sync.gist": "1a31655225611866113c897352f50eab",
+    "sync.host": "",
+    "sync.pathPrefix": "",
+    "sync.quietSync": false,
+    "sync.askGistName": false,
+    "sync.removeExtensions": true,
+    "sync.syncExtensions": true,
+    "sync.autoDownload": false,
+    "sync.autoUpload": false,
+    "sync.lastUpload": "",
+    "sync.lastDownload": "2018-07-02T16:32:28.221Z",
+    "sync.forceDownload": false,
+    "gitlens.advanced.messages": {
+        "suppressShowKeyBindingsNotice": true
+    },
+    "workbench.colorTheme": "Monokai",
+    "terminal.integrated.fontFamily": "Ubuntu mono",
+    "editor.formatOnSave": true,
+    "editor.formatOnPaste": true,
+    "css.lint.hexColorLength": "error",
+}
