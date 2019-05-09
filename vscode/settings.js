d4465a920906a3ef1140dd9dec573ddc6d44a21e
michalhonc <honc355@gmail.com>
2019-05-09 08:58:01 +0200

    Update settings.js

--- vscode/settings.js
+++ vscode/settings.js
@@ -9,38 +9,48 @@
     "editor.minimap.enabled": true,
     "editor.tabSize": 4,
     "editor.wordWrap": "on",
-    "workbench.iconTheme": "vscode-icons",
     "editor.snippetSuggestions": "top",
     "editor.cursorStyle": "line-thin",
     "editor.selectionClipboard": false,
-    "editor.smoothScrolling": true,
     "editor.acceptSuggestionOnCommitCharacter": false,
+    "editor.cursorBlinking": "phase",
+    "editor.cursorSmoothCaretAnimation": true,
+    "editor.parameterHints.enabled": false,
+    "editor.suggest.snippetsPreventQuickSuggestions": false,
+    // minimap
+        "editor.minimap.renderCharacters": false,
+        "editor.minimap.maxColumn": 100,
+        "editor.minimap.showSlider": "always",
     // WORKBENCH
     "workbench.panel.defaultLocation": "right",
     "workbench.editor.showTabs": true,
     "workbench.colorTheme": "Monokai",
-    "workbench.activityBar.visible": true,
+    "workbench.activityBar.visible": false,
     "workbench.startupEditor": "newUntitledFile",
     // FILES
     "files.autoSave": "afterDelay",
     "files.autoSaveDelay": 10000,
+    "files.insertFinalNewline": true,
+    "files.trimTrailingWhitespace": true,
     // OTHERS
     "emmet.includeLanguages": {
         "javascript": "javascriptreact"
     },
     "breadcrumbs.enabled": true,
     "explorer.confirmDragAndDrop": false,
-
+    "telemetry.enableCrashReporter": false,
+    "workbench.settings.enableNaturalLanguageSearch": false,
+    "workbench.statusBar.feedback.visible": false,
+    "telemetry.enableTelemetry": false,
     // PLUGINS
-    "vsicons.dontShowNewVersionMessage": true,
     "eslint.enable": true,
     "css.lint.hexColorLength": "error",
     "gitlens.advanced.messages": {
         "suppressShowKeyBindingsNotice": true
     },
-    "javascript.updateImportsOnFileMove.enabled": "always",
-    "reason.path.ocamlmerlin": "/home/michalhonc/.opam/default/bin/ocamlmerlin",
     "postcssSorting.config": {
         "properties-order": ["display","float","position","left","right","top","bottom","order","align-self","z-index","clear","box-sizing","margin","margin-top","margin-right","margin-bottom","margin-left","border","border-top","border-right","border-bottom","border-left","border-radius","padding","padding-top","padding-right","padding-bottom","padding-left","width","min-width","max-width","height","min-height","max-height","flex","flex-grow","flex-shrink","flex-basis","overflow","flex","flex-flow","flex-direction","flex-wrap","justify-content","align-content","align-items","column-count","column-width","column-gap","column-rule","column-fill","column-span","object-position","object-fit","content","text-indent","font","line-height","text-align","vertical-align","color","white-space","text-overflow","text-transform","text-decoration","text-shadow","background","background-color","background-image","background-repeat","background-attachment","background-position","list-style","box-shadow","transform","animation","transition","appearance","cursor","visibility","opacity",],
     },
+    "liveshare.featureSet": "insiders",
+    "workbench.iconTheme": "vscode-icons"
 }
