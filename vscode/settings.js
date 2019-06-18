ce4fecb14a80da432e69b7de23e0906a21c825b4
michalhonc <honc355@gmail.com>
2019-06-18 08:28:35 +0200

    Vim update

--- vscode/settings.js
+++ vscode/settings.js
@@ -1,11 +1,23 @@
 {
+    // VIM
+    "vim.easymotion": true,
+    "vim.useSystemClipboard": true,
+    "vim.leader": "<space>",
+    "vim.handleKeys": {
+        "<C-e>": false,
+    },
+    "vim.hlsearch": true,
+    "vim.incsearch": true,
+
     // WINDOW
     "window.zoomLevel": 0,
     "window.menuBarVisibility": "default",
     "window.title": "${dirty} ${activeEditorMedium}${separator}${rootName}",
     "window.titleBarStyle": "custom",
+
     // EDITOR
     "editor.fontFamily": "Consolas, 'Courier New', monospace",
+    "editor.lineNumbers": "relative",
     "editor.minimap.enabled": true,
     "editor.tabSize": 4,
     "editor.wordWrap": "on",
@@ -17,10 +29,12 @@
     "editor.cursorSmoothCaretAnimation": true,
     "editor.parameterHints.enabled": false,
     "editor.suggest.snippetsPreventQuickSuggestions": false,
-    // minimap
-        "editor.minimap.renderCharacters": false,
-        "editor.minimap.maxColumn": 100,
-        "editor.minimap.showSlider": "always",
+
+// minimap
+"editor.minimap.renderCharacters": false,
+    "editor.minimap.maxColumn": 100,
+    "editor.minimap.showSlider": "always",
+
     // WORKBENCH
     "workbench.panel.defaultLocation": "right",
     "workbench.editor.showTabs": true,
@@ -52,5 +66,60 @@
         "properties-order": ["display","float","position","left","right","top","bottom","order","align-self","z-index","clear","box-sizing","margin","margin-top","margin-right","margin-bottom","margin-left","border","border-top","border-right","border-bottom","border-left","border-radius","padding","padding-top","padding-right","padding-bottom","padding-left","width","min-width","max-width","height","min-height","max-height","flex","flex-grow","flex-shrink","flex-basis","overflow","flex","flex-flow","flex-direction","flex-wrap","justify-content","align-content","align-items","column-count","column-width","column-gap","column-rule","column-fill","column-span","object-position","object-fit","content","text-indent","font","line-height","text-align","vertical-align","color","white-space","text-overflow","text-transform","text-decoration","text-shadow","background","background-color","background-image","background-repeat","background-attachment","background-position","list-style","box-shadow","transform","animation","transition","appearance","cursor","visibility","opacity",],
     },
     "liveshare.featureSet": "insiders",
+    "gitlens.mode.active": "review",
+    "peacock.favoriteColors": [
+        {
+            "name": "Angular Red",
+            "value": "#b52e31"
+        },
+        {
+            "name": "Auth0 Orange",
+            "value": "#eb5424"
+        },
+        {
+            "name": "Azure Blue",
+            "value": "#007fff"
+        },
+        {
+            "name": "C# Purple",
+            "value": "#68217A"
+        },
+        {
+            "name": "Gatsby Purple",
+            "value": "#639"
+        },
+        {
+            "name": "Go Cyan",
+            "value": "#5dc9e2"
+        },
+        {
+            "name": "Java Blue-Gray",
+            "value": "#557c9b"
+        },
+        {
+            "name": "JavaScript Yellow",
+            "value": "#f9e64f"
+        },
+        {
+            "name": "Mandalorian Blue",
+            "value": "#1857a4"
+        },
+        {
+            "name": "Node Green",
+            "value": "#215732"
+        },
+        {
+            "name": "React Blue",
+            "value": "#00b3e6"
+        },
+        {
+            "name": "Something Different",
+            "value": "#832561"
+        },
+        {
+            "name": "Vue Green",
+            "value": "#42b883"
+        }
+    ],
     "workbench.iconTheme": "vscode-icons"
 }
