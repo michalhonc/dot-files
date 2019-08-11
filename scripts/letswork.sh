eadec9485a9f3ca1d00e86472dccf3c14874f762
michalhonc <honc355@gmail.com>
2019-08-11 10:52:16 +0200

    Update letswork.sh

--- scripts/letswork.sh
+++ scripts/letswork.sh
@@ -1,24 +1,13 @@
+#!/bin/bash
 # script that launch work application
-#! bin/sh
 
 # Launch scripts
 xkbset ma 1 10 30 30 1 &
 
 # Launch apps
 google-chrome &
-wmctrl -r chrome -t 1 &
-code ./git/workspace/renderer.code-workspace &
+simplenote &
+# wmctrl -r chrome -t 1 &
 evolution &
-pidgin &
 mattermost-desktop &
-
-# Guake Terminal
-
-guake -n ~/git/renderer --rename-current-tab="renderer"
-
-tmux split-window -h -c "#{pane_current_path}" \; send-keys "npm run build:dev-disc -https" Enter &
-
-/bin/sleep 60
-
-tmux split-window -v -c "#{pane_current_path}" \; send-keys "SERVICE_HTTPS=true SERVICE_PORT_HTTPS=10504 npm run server:dev" Enter &
-
+#gnome-pomodoro &
